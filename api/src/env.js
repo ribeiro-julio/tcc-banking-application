06a6960346ce83eb1447b0a8c79572dc36d948a8
Julio Ribeiro <julio.ribeiro303@gmail.com>
2023-10-02 23:22:32 -0300

    update env

--- api/src/env.js
+++ api/src/env.js
@@ -1,10 +1,6 @@
 // app
 export const PORT = parseInt(process.env.PORT) || -1;
 
-// database
-export const DEFAULT_USER_PASSWORD = process.env.DEFAULT_USER_PASSWORD || "";
-export const DEFAULT_USER_PIN = process.env.DEFAULT_USER_PIN || "";
-
 // encryption
 export const TOTP_SECRET_ENCRYPTION_KEY =
   process.env.TOTP_SECRET_ENCRYPTION_KEY || "";
